640edeebc1c63aed53e4e0e05b9bc05cfd6b8f9c
Minusha Shaik <minusha.shaik@gmail.com>
2021-10-28 20:32:27 -0700

    midterm changes

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Minusha:admin@cluster0.ojxql.mongodb.net/book_store?retryWrites=true&w=majority"//database for booklist
+};
